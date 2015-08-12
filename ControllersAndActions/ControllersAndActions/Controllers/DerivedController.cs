b970443e13fff80add17bb0222bae39e1217b4df
Rong Zhuang <jojozhuang@gmail.com>
2015-08-12 14:12:16 -0500

    finished controller

--- ControllersAndActions/ControllersAndActions/Controllers/DerivedController.cs
+++ ControllersAndActions/ControllersAndActions/Controllers/DerivedController.cs
@@ -3,17 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ControllersAndActions.Infrastructure;
 
 namespace ControllersAndActions.Controllers
 {
     public class DerivedController : Controller
     {
-        //
-        // GET: /Derived/
         public ActionResult Index()
         {
             ViewBag.Message = "Hello from the DerivedController Index method";
             return View("MyView");
+        }
+
+        public ActionResult ProduceOutput()
+        {
+            //if (Server.MachineName == "CACSWS13")
+            //{
+            //    //Response.Redirect("/Basic/Index");
+            //    return new CustomRedirectResult { Url = "/Basic/Index" };
+            //}
+            //else
+            //{
+            //    Response.Write("Controller: Derived, Action: ProduceOutput");
+            //    return null;
+            //}
+            
+            return new RedirectResult("/Basic/Indexdd");
+
+            //return Redirect("/Basic/Index");
         }
 	}
 }
---
+++ ControllersAndActions/ControllersAndActions/Controllers/ExampleController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControllersAndActions.Controllers
+{
+    public class ExampleController : Controller
+    {
+        public ViewResult Index()
+        {
+            //TempData.Keep("Date");
+            DateTime time = (DateTime)TempData.Peek("Date");
+            ViewBag.Message = TempData["Message"];
+            TempData.Keep("Message");
+            ViewBag.Date = TempData["Date"];
+            TempData.Keep("Date");
+            return View();
+        }
+
+        public RedirectResult Redirect()
+        {
+            return Redirect("/Example/Index");
+        }
+
+        public RedirectToRouteResult Redirect2()
+        {
+            return RedirectToRoute(new
+            {
+                controller = "Example",
+                action = "Index",
+                ID = "MyID"
+            });
+        }
+
+        public RedirectToRouteResult RedirectToRoute()
+        {
+            TempData["Message"] = "Hello";
+            TempData["Date"] = DateTime.Now;
+            return RedirectToAction("Index");
+        }
+
+        public HttpStatusCodeResult StatusCode()
+        {
+            return new HttpStatusCodeResult(404, "URL cannot be serviced");
+        }
+    }
+}
