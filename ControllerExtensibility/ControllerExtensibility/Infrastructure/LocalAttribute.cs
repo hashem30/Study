2c715939084bb742bead42719a05b4631925bf2b
Rong Zhuang <jojozhuang@gmail.com>
2015-08-13 17:33:07 -0500

    study views begin

---
+++ ControllerExtensibility/ControllerExtensibility/Infrastructure/LocalAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace ControllerExtensibility.Infrastructure
+{
+    public class LocalAttribute : ActionMethodSelectorAttribute 
+    {
+        public override bool IsValidForRequest(ControllerContext controllerContext, MethodInfo methodInfo)
+        {
+            return controllerContext.HttpContext.Request.IsLocal;
+        }
+    }
+}
