a1e5611868a325d64261fd8179370c9b8dff62ab
jojozhuang <jojozhuang@gmail.com>
2016-05-15 23:51:33 -0500

    begin whiteboard

--- NodeJs/Buffer/colds.js
+++ NodeJs/Buffer/colds.js
@@ -56,6 +56,35 @@ exports.getImageData = function (second, callback) {
 
 }
 
+exports.getWhiteBoardData = function (second, callback) {
+  //filehelper.unzipIndexFile(indexfile, unzippedfile);
+  var buffer = filehelper.getIndexFile(unzippedfile);
+  //console.log(buffer.length);
+  //console.log(buffer);
+  var indexList = filehelper.getIndexArray(buffer);
+  //console.log(indexList.length);
+  var count =0;
+  var hashmap = [];
+  for (var i = 0; i < indexList.length; i++)
+  {
+    if(!hashmap[indexList[i].timestamp]) {
+      count++;
+      hashmap[indexList[i].timestamp] = i;
+      ///console.log('hashmap.length=' + hashmap.length);
+      //if (count < 10) {
+      //  console.log(indexList[i].timestamp + '-' + i);
+        //console.log(hashmap[indexList[i].timestamp]);
+      //}
+    }
+  }
+
+
+  var imageIndex = filehelper.getImageIndex(hashmap, indexList, second);
+
+  filehelper.getImageData(imagedatafile, imageIndex, callback);
+
+}
+
 function sortNumber(a,b) {
     return a - b;
 }
